4789f51bcc05c6f4764a465fb9b9ff7b05ffe287
hngphanminh147 <hngphanminh147@gmail.com>
2023-02-08 10:11:46 +0700

    Add AOP logging

--- uaa/src/main/java/com/self/uaa/constants/Constants.java
+++ uaa/src/main/java/com/self/uaa/constants/Constants.java
@@ -4,6 +4,5 @@ public final class Constants {
     // Exception error messages
     public static final String USERNAME_NOT_FOUND = "Username not found";
     public static final String USERNAME_NOT_FOUND_FORMAT = "Username {%s} not found";
-
     public static final String USERNAME_IS_EMPTY = "Username is empty";
 }
